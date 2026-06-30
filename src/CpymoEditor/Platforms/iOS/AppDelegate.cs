7f98991a09e4f0337f1c3381bba29e59c65f281b
boz700908 <3468806165@qq.com>
2026-06-30 22:42:09 +0800

    chore: remove unused maui platforms

--- src/CpymoEditor/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CpymoEditor;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
